d8afea4f8c6be756a5d9ae2f4e9e0dae7a67dc5f
Sharllack <164765027+Sharllack@users.noreply.github.com>
2024-05-13 19:40:13 -0300

    Mudanças

--- JS.index/darkmode.js
+++ JS.index/darkmode.js
@@ -1,16 +1,28 @@
 // JavaScript para detectar a mudança e executar uma ação
 document.getElementById('toggle').addEventListener('change', function() {
     main = document.getElementById('main');
+    topo = document.getElementById('topo');
+    log = document.getElementById('topolog');
+    cad = document.getElementById('topocad');
+    usu = document.getElementById('usu');
     if(this.checked) {
       console.log('Toggle ON');
       main.style.backgroundColor = "black";
       main.style.transition = '.5s';
-      prod.style.backgroundColor = "black";
+      topo.style.backgroundColor = "black";
+      topo.style.transition = '.5s';
+      log.style.color = "white";
+      cad.style.color = "white";
+      usu.style.backgroundColor = "black";
     } else {
       console.log('Toggle OFF');
       main.style.backgroundColor = "";
       main.style.transition = '.5s';
-      prod.style.backgroundColor = "";
+      topo.style.backgroundColor = "";
+      topo.style.transition = '.5s';
+      log.style.color = "";
+      cad.style.color = ""
+      usu.style.backgroundColor = "rgb(212, 212, 212)";
     }
   });
 
