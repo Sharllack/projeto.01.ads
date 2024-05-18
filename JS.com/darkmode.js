8d36676af91ddcb249349a00e59ed06b8d4b88f2
Sharllack <164765027+Sharllack@users.noreply.github.com>
2024-05-18 13:36:37 -0300

    dark mode em tudo

--- JS.com/darkmode.js
+++ JS.com/darkmode.js
@@ -1,11 +1,11 @@
-document.querySelector('.toggle').addEventListener('change', function() {
-    const topo = document.getElementById('topo');
-    const header = document.getElementById('header');
-    const log = document.querySelector('.topolog');
-    const cad = document.querySelector('.topocad');
-    const usu = document.querySelector('#usu');
-    const logado = document.getElementById('logado');
-  
+const topo = document.getElementById('topo');
+const header = document.getElementById('header');
+const log = document.querySelector('.topolog');
+const cad = document.querySelector('.topocad');
+const usu = document.querySelector('#usu');
+const logado = document.getElementById('logado');
+
+document.querySelector('.toggle').addEventListener('change', function() {  
     if (this.checked) {
         console.log('Toggle ON');
         topo.style.backgroundColor = "black";
@@ -20,7 +20,9 @@ document.querySelector('.toggle').addEventListener('change', function() {
         usu.style.transition = '.5s';
         logado.style.backgroundColor = "black";
         logado.style.transition = '.5s';
-        
+
+        localStorage.setItem('darkMode', 'on');
+
     } else {
         console.log('Toggle OFF');
         topo.style.backgroundColor = "";
@@ -35,5 +37,38 @@ document.querySelector('.toggle').addEventListener('change', function() {
         usu.style.transition = '.5s';
         logado.style.backgroundColor = "";
         logado.style.transition = '.5s';
+
+        localStorage.setItem('darkMode', 'off');
+
   };
   });
+
+  const darkModePreference = localStorage.getItem('darkMode');
+  if (darkModePreference === 'on') {
+    topo.style.backgroundColor = "black";
+    topo.style.transition = '.5s';
+    header.style.backgroundColor = "black"
+    header.style.transition = ".5s";
+    log.style.color = "white";
+    log.style.transition = '.5s';
+    cad.style.color = "white";
+    cad.style.transition = '.5s';
+    usu.style.backgroundColor = "black";
+    usu.style.transition = '.5s';
+    logado.style.backgroundColor = "black";
+    logado.style.transition = '.5s';
+      
+  } else {
+    topo.style.backgroundColor = "";
+    topo.style.transition = '.5s';
+    header.style.backgroundColor = ""
+    header.style.transition = ".5s";
+    log.style.color = "";
+    log.style.transition = '.5s';
+    cad.style.color = "";
+    cad.style.transition = '.5s';
+    usu.style.backgroundColor = "";
+    usu.style.transition = '.5s';
+    logado.style.backgroundColor = "";
+    logado.style.transition = '.5s';
+  }
--- JS.index/darkmode.js
+++ JS.index/darkmode.js
@@ -1,12 +1,13 @@
-document.querySelector('.toggle').addEventListener('change', function() {
-  const main = document.getElementById('main');
-  const topo = document.getElementById('topo');
-  const header = document.getElementById('header');
-  const log = document.querySelector('.topolog');
-  const cad = document.querySelector('.topocad');
-  const usu = document.querySelector('#usu');
-  const logado = document.getElementById('logado');
+const main = document.getElementById('main');
+const topo = document.getElementById('topo');
+const header = document.getElementById('header');
+const log = document.querySelector('.topolog');
+const cad = document.querySelector('.topocad');
+const usu = document.querySelector('#usu');
+const logado = document.getElementById('logado');
+const icon =document.querySelector('.material-symbols-outlined');
 
+document.querySelector('.toggle').addEventListener('change', function() {
   if (this.checked) {
       console.log('Toggle ON');
       main.style.backgroundColor = "black";
@@ -23,6 +24,10 @@ document.querySelector('.toggle').addEventListener('change', function() {
       usu.style.transition = '.5s';
       logado.style.backgroundColor = "black";
       logado.style.transition = '.5s';
+      icon.style.color = "white";
+      icon.style.transition = '.5s';
+
+      localStorage.setItem('darkMode', 'on');
       
   } else {
       console.log('Toggle OFF');
@@ -40,5 +45,48 @@ document.querySelector('.toggle').addEventListener('change', function() {
       usu.style.transition = '.5s';
       logado.style.backgroundColor = "";
       logado.style.transition = '.5s';
+      icon.style.color = "";
+      icon.style.transition = '.5s';
+
+      localStorage.setItem('darkMode', 'off');
+
 };
 });
+
+const darkModePreference = localStorage.getItem('darkMode');
+if (darkModePreference === 'on') {
+    main.style.backgroundColor = "black";
+    main.style.transition = '.5s';
+    topo.style.backgroundColor = "black";
+    topo.style.transition = '.5s';
+    header.style.backgroundColor = "black"
+    header.style.transition = ".5s";
+    log.style.color = "white";
+    log.style.transition = '.5s';
+    cad.style.color = "white";
+    cad.style.transition = '.5s';
+    usu.style.backgroundColor = "black";
+    usu.style.transition = '.5s';
+    logado.style.backgroundColor = "black";
+    logado.style.transition = '.5s';
+    icon.style.color = "white";
+    icon.style.transition = '.5s';
+    
+} else {
+    main.style.backgroundColor = "";
+    main.style.transition = '.5s';
+    topo.style.backgroundColor = "";
+    topo.style.transition = '.5s';
+    header.style.backgroundColor = ""
+    header.style.transition = ".5s";
+    log.style.color = "";
+    log.style.transition = '.5s';
+    cad.style.color = "";
+    cad.style.transition = '.5s';
+    usu.style.backgroundColor = "";
+    usu.style.transition = '.5s';
+    logado.style.backgroundColor = "";
+    logado.style.transition = '.5s';
+    icon.style.color = "";
+    icon.style.transition = '.5s';
+}
